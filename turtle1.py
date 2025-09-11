7adec6849086f6e8e3cd9d30a2beb94f1732f46b
haneefhaneef <haneeehaneee2007@gmail.com>
2025-09-11 18:07:39 +0530

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.setup(800,600)
+window=turtle.Screen()
+window.title("absolute positioning")
+tt=turtle.getturtle()
+tt.hideturtle()
+tt.setposition(100,0)
+tt.setposition(100,100)
+tt.setposition(0,100)
+tt.setposition(0,0)
+turtle.exitonclick()
+
