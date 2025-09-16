fd34e3f8017338b0c68dac8443742d014253a172
haneefhaneef <haneeehaneee2007@gmail.com>
2025-09-16 15:09:13 +0530

    Add files via upload

---
+++ try,except,else,finaly.py
@@ -0,0 +1,51 @@
+try:
+    n=int(input("enter a value:"))
+    res=50/n
+except zerodivisionerror:
+    print("zero division error")
+else:
+    print(res)
+
+finally:
+    print("bye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
